3505538cd33bd995afc9f5e38b4b549fd7642937
thereallee <98909739+thereallee@users.noreply.github.com>
2025-10-21 19:28:19 -0400

    got the frame reader to compile

--- SimplePlayer/FrameReader.swift
+++ SimplePlayer/FrameReader.swift
@@ -16,10 +16,42 @@ class FrameReader {
         let context = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
         
         var audioFileStream: AudioFileStreamID?
-        let status = AudioFileStreamOpen(context, propertyListener, packetsCallback, kAudioFileMP3Type, &audioFileStream)
         
-        let status2 = AudioFileStreamOpen(context, propertyListener, packetsCallback, kAudioFileMP3Type, &audioFileStream)
+//        let status = AudioFileStreamOpen(context, propertyListener, packetsCallback, kAudioFileMP3Type, &audioFileStream)
         
+        
+        
+        let status = AudioFileStreamOpen(context,
+                                          
+            {inClientData, inAudioFileStream, inPropertyID,
+            ioFlags in
+                    // This callback is called when properties of the audio stream are discovered.
+                // For example, you can query for the AudioStreamBasicDescription here.
+                if inPropertyID == kAudioFileStreamProperty_DataFormat {
+                    // You can check the format here
+                }
+            }
+        ,
+          
+          {inUserData, inNumberBytes, inNumberPackets,
+            inInputData, inPacketDescriptions in
+
+            guard let inPacketDescriptions else {
+                
+                return
+                
+            }
+            // This callback is where you get the raw audio packets (frames).
+            let packetSize = Int(inPacketDescriptions.pointee.mDataByteSize)
+            let packetOffset = Int(inPacketDescriptions.pointee.mStartOffset)
+                
+            let frameData = Data(bytes: inInputData.advanced(by: packetOffset), count: packetSize)
+            // Process the frameData here
+            print("Read MP3 frame with size: \(packetSize)")
+        },
+                                          kAudioFileMP3Type,
+                                          &audioFileStream)
+            
         guard status == noErr, let streamID = audioFileStream else {
             print("Failed to open audio file stream: \(status)")
             return
@@ -36,27 +68,30 @@ class FrameReader {
     }
 
     // A C function required by the AudioFileStream API
-    func propertyListener(_ inClientData: UnsafeMutableRawPointer, _ inAudioFileStream: AudioFileStreamID, _ inPropertyID: AudioFileStreamPropertyID, _ ioFlags: UnsafeMutablePointer<AudioFileStreamPropertyFlags>) {
-        // This callback is called when properties of the audio stream are discovered.
-        // For example, you can query for the AudioStreamBasicDescription here.
-        if inPropertyID == kAudioFileStreamProperty_DataFormat {
-            // You can check the format here
-        }
-    }
+//    func  propertyListener (_ inClientData: UnsafeMutableRawPointer, _ inAudioFileStream: AudioFileStreamID, _ inPropertyID: AudioFileStreamPropertyID, _ ioFlags: UnsafeMutablePointer<AudioFileStreamPropertyFlags>) {
+//        // This callback is called when properties of the audio stream are discovered.
+//        // For example, you can query for the AudioStreamBasicDescription here.
+//        if inPropertyID == kAudioFileStreamProperty_DataFormat {
+//            // You can check the format here
+//        }
+//    }
 
     // A C function required by the AudioFileStream API
-    func packetsCallback(_ inClientData: UnsafeMutableRawPointer, _ inNumberPacketDescriptions: UInt32, _ inInputData: UnsafeRawPointer, _ inPacketDescriptions: UnsafeMutablePointer<AudioStreamPacketDescription>) {
-        // This callback is where you get the raw audio packets (frames).
-        for i in 0..<inNumberPacketDescriptions {
-            let packetDescription = inPacketDescriptions[Int(i)]
-            let packetSize = Int(packetDescription.mDataByteSize)
-            let packetOffset = Int(packetDescription.mStartOffset)
-            
-            let frameData = Data(bytes: inInputData.advanced(by: packetOffset), count: packetSize)
-            // Process the frameData here
-            print("Read MP3 frame with size: \(packetSize)")
-        }
-    }
+//    func packetsCallback (_ inClientData: UnsafeMutableRawPointer, _ inNumberPacketDescriptions: UInt32,  _ unKnownYetInt32: UInt32,
+//                         _ inInputData: UnsafeRawPointer,
+//                          _ inPacketDescriptions: Optional<UnsafeMutablePointer<AudioStreamPacketDescription>>) {
+//        // This callback is where you get the raw audio packets (frames).
+//        for i in 0..<inNumberPacketDescriptions {
+//            // TODO think about the optional here
+//            let packetDescription = inPacketDescriptions![Int(i)]
+//            let packetSize = Int(packetDescription.mDataByteSize)
+//            let packetOffset = Int(packetDescription.mStartOffset)
+//            
+//            let frameData = Data(bytes: inInputData.advanced(by: packetOffset), count: packetSize)
+//            // Process the frameData here
+//            print("Read MP3 frame with size: \(packetSize)")
+//        }
+//    }
 
     // Example usage:
     // guard let url = Bundle.main.url(forResource: "my_song", withExtension: "mp3") else { return }
